539cda85ea7a33f01802e21b02613f49fa13871d
Mark Maligalig <maligali@ualberta.ca>
2023-10-13 00:59:17 -0600

    Added most GET functions in httpclient; added license

--- httpclient.py
+++ httpclient.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
+# Copyright 2023 Abram Hindle, https://github.com/tywtyw2002, https://github.com/treedust, Mark Maligalig
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -24,6 +24,8 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+DEFAULT_PORT = 80 # Use if port is not explicitly given
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
 
@@ -33,10 +35,25 @@ class HTTPResponse(object):
         self.body = body
 
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    def get_host_port(self,url):
+        # Check if port is explictly written
+        # Check for host; return host and port
+        # (^\/\/|^@)?[a-zA-Z0-9.]+(^:|^\/)? to isolate parts of url
+        #TODO: Clarify if host = www.google.com OR www.google.com/path
+        #TODO: what about if host = [::1]?
+        # Assuming every url starts with scheme://
+        host_port = url.split("/")[2].split(":") # Get host:port
+        host = host_port[0]
+
+        if ":" in host_port:
+            port = re.findall('', url)
+        else:
+            port = DEFAULT_PORT
+
+        return host, port
 
     def connect(self, host, port):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Open socket using IPv4 and TCP
         self.socket.connect((host, port))
         return None
 
@@ -44,10 +61,17 @@ class HTTPClient(object):
         return None
 
     def get_headers(self,data):
-        return None
+        # TODO: Do headers need to be stored in dict?
+        headers = data.split("\r\n\r\n")[0]
+        return headers
 
     def get_body(self, data):
-        return None
+        # TODO: Body = after headers?
+        info = data.split("\r\n\r\n")
+        body = None
+        if len(info) > 1:
+            body = info[1]
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -70,6 +94,24 @@ class HTTPClient(object):
     def GET(self, url, args=None):
         code = 500
         body = ""
+
+        request = "GET \ HTTP/1.1\nHost: " + host + "\n\n"
+
+        host, port = self.get_host_port(url)
+        self.connect(host, port) # Connect to server
+        self.sendall(request) # Send request to server
+
+        self.socket.shutdown(socket.SHUT_WR) # Tell server socket is done sending!
+
+        data = self.recvall(self.socket)
+        #print(data) # User story 5
+
+        print(self.get_headers(data))
+
+        self.socket.close()
+
+
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
